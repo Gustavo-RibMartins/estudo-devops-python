7278fbbf42eaae3f9e34e363a36eb366f9943509
Gustavo Ribeiro <guhrm96@gmail.com>
2022-10-18 22:05:40 -0300

    Conclusão da aula 01

---
+++ Curso-Impacta/Aula01-Introducao.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+type (62) # Para mostrar a tipagem da variavel
+
+dir(62) # Para mostrar os metodos que se aplicam a variavel
+
+print('String') # Strings sao delimitadas por aspas duplas ou simples
+
+help('Teste'.upper) # Help(<var>.Metodo) mostra a documentacao do metodo
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # estudo-devops-python
-Repositório para estudo da linguagem Python.
+
+> Repositório para estudo da linguagem Python.
+
+---
+
+
